c77f9a0ee2a4070bfb844cb89c4e5564c952d2b9
GreenVenom77 <hosome2222@gmail.com>
2024-03-24 14:08:07 +0200

    New layout that I really like

--- app/src/main/java/com/example/imagespace/MainActivity.kt
+++ app/src/main/java/com/example/imagespace/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.example.imagespace
 
-import android.graphics.drawable.PaintDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -60,8 +62,8 @@ fun getSpaceInfo(index: Int): String {
 @Composable
 fun getSpaceImage(index: Int): Painter {
 
-    val image: Painter = when(index) {
-        1 -> painterResource(R.drawable.ic_launcher_background)
+        val image: Painter = when(index) {
+        1 -> painterResource(R.drawable._209063)
 
         else -> painterResource(R.drawable.ic_launcher_background)
     }
@@ -77,35 +79,62 @@ fun SpaceView(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
             .fillMaxSize()
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-        ) {
-            Image(
-                painter = getSpaceImage(1),
-                contentDescription = "Nothing"
-            )
-            Spacer(modifier = Modifier.height(50.dp))
-            Text(
-                text = getSpaceInfo(1)
-            )
-        }
         Row(
-            verticalAlignment = Alignment.Bottom,
+            verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceEvenly,
             modifier = modifier
-                .fillMaxWidth()
+                .fillMaxSize()
         ) {
-            Button(
-                onClick = { /*TODO*/ },
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
+                modifier = modifier
+                    .fillMaxHeight()
+                    .weight(0.5f)
             ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                ) {
 
+                }
             }
-
-            Button(
-                onClick = { /*TODO*/ },
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .weight(2f)
             ) {
+                Box(
+                    modifier = Modifier
+                        .aspectRatio(0.5f)
+                        .fillMaxSize()
+                ) {
+                    Image(
+                        painter = getSpaceImage(1),
+                        contentDescription = "Nothing",
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                    )
+                }
 
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = getSpaceInfo(1)
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End,
+                modifier = modifier
+                    .fillMaxHeight()
+                    .weight(0.5f)
+            ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                ) {
+
+                }
             }
         }
     }
